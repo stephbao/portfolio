300c07bbe952140c07006f5cc920a73bde310124
stephbao <szbao_97@live.com>
2017-11-27 20:20:32 -0500

    update

--- page.js
+++ page.js
@@ -11,3 +11,18 @@ function closeNav() {
     document.getElementById("open").style.display = "block";
     document.getElementById("close").style.display = "none";
 }
+
+$(document).ready(function(){
+  // Add smooth scrolling to all links
+  $("a").on('click', function(event) {
+    if (this.hash !== "") {
+      event.preventDefault();
+      var hash = this.hash;
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 700, function(){
+        window.location.hash = hash;
+      });
+    } // End if
+  });
+});
