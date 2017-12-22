028e208ab3caca7fcfa8124d2bde1851242a6eab
stephbao <szbao_97@live.com>
2017-12-22 01:39:04 -0500

    changes

--- page.js
+++ page.js
@@ -3,6 +3,10 @@ function openHome() {
     document.getElementById("about").style.display = "none";
     document.getElementById("portfolio").style.display = "none";
     document.getElementById("contact").style.display = "none";
+    document.getElementById("home").style.display = "block";
+    document.getElementById("cmlh").style.display = "none";
+    document.getElementById("todo").style.display = "none";
+
 
     document.getElementById("linkabout").style.color = "dimgrey";
     document.getElementById("linkportfolio").style.color = "dimgrey";
@@ -15,6 +19,9 @@ function openAbout() {
     document.getElementById("about").style.display = "block";
     document.getElementById("portfolio").style.display = "none";
     document.getElementById("contact").style.display = "none";
+    document.getElementById("home").style.display = "none";
+    document.getElementById("cmlh").style.display = "none";
+    document.getElementById("todo").style.display = "none";
 
 
     document.getElementById("linkabout").style.color = "#EB8888";
@@ -27,6 +34,10 @@ function openPortfolio() {
     document.getElementById("about").style.display = "none";
     document.getElementById("portfolio").style.display = "block";
     document.getElementById("contact").style.display = "none";
+    document.getElementById("home").style.display = "none";
+    document.getElementById("cmlh").style.display = "none";
+    document.getElementById("todo").style.display = "none";
+
 
     document.getElementById("linkabout").style.color = "dimgrey";
     document.getElementById("linkportfolio").style.color = "#EB8888";
@@ -38,8 +49,21 @@ function openContact() {
     document.getElementById("about").style.display = "none";
     document.getElementById("portfolio").style.display = "none";
     document.getElementById("contact").style.display = "block";
+    document.getElementById("home").style.display = "none";
+    document.getElementById("cmlh").style.display = "none";
+    document.getElementById("todo").style.display = "none";
 
     document.getElementById("linkabout").style.color = "dimgrey";
     document.getElementById("linkportfolio").style.color = "dimgrey";
     document.getElementById("linkcontact").style.color = "#EB8888";
+}
+
+function pCMLH() {
+    document.getElementById("cmlh").style.display = "block";
+    document.getElementById("portfolio").style.display = "none";
+}
+
+function pTODO() {
+    document.getElementById("todo").style.display = "block";
+    document.getElementById("portfolio").style.display = "none";
 }
